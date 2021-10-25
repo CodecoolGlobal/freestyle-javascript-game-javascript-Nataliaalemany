36c9ff4ee04e112715f71eb64ce24fdc0bf179b6
Csupihun <csupihun@gmail.com>
2021-10-25 12:45:54 +0200

    add starting state

--- game.js
+++ game.js
@@ -1,7 +1,15 @@
-initGame();
+// initGame();
 
 function initGame() {
-
     // Your game can start here, but define separate functions, don't write everything in here :)
-
+    create_menu()
 }
+
+function create_menu(){
+    let body = document.body
+    body.style.backgroundImage = "url(pictures/bg.png)"
+    body.style.backgroundRepeat = "no-repeat"
+    body.style.maxHeight = "100vh"
+    body.style.maxWidth = "100%"
+}
+
