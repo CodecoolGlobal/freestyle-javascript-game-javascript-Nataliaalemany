6fb272fe36487b0b6c48a804315eda54700f829e
Natalia Alemany <nataliaalemanybota@gmail.com>
2021-10-25 14:42:52 +0200

    add buttons centered

--- game.js
+++ game.js
@@ -2,14 +2,13 @@
 
 function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
-    create_menu()
+    createMenu()
 }
 
-function create_menu(){
+function createMenu(){
     let body = document.body
     body.style.backgroundImage = "url(pictures/bg.png)"
     body.style.backgroundRepeat = "no-repeat"
     body.style.maxHeight = "100vh"
     body.style.maxWidth = "100%"
 }
-
