10634a001ef8d367a4e778902c4eedc37487a97a
Csupihun <csupihun@gmail.com>
2021-10-27 18:56:45 +0200

    Add first steps on enemy movement

--- static/game.js
+++ static/game.js
@@ -7,20 +7,44 @@ const level = document.getElementById('theme/lvl').innerText.split(' ')[1]
 const battleGround = document.getElementById("battleground")
 // level resources
 const lotr = {enemies: enemies.lotrEnemies, lvl1: 'BackgroundOne.png', lvl2: 'BackgroundTwo.jpg', lvl3: 'BackgroundThree.png'}
-const shrek = {enemies: enemies.shrekEnemies}
+const shrek = {enemies: enemies.shrekEnemies, lvl1: 'BackgroundOne.png', lvl2: 'BackgroundTwo.jpg', lvl3: 'BackgroundThree.jpg'}
 const marvel = {enemies: enemies.marvelEnemies}
 let enemyCount = 0
 const difficulty = "normal"
+let moveOrder = 0
+let enemyIds = []
+let score = 0
+let health = 0
+let spawnedEnemies = 0
 
 
 function initGame() {
-    // Your game can start here, but define separate functions, don't write everything in here :)
     let playingTheme = getTheme()
     playingTheme = getLevelData(playingTheme)
     document.body.style.backgroundImage = `url("/static/pictures/${theme}/${playingTheme.bgimage}")`
     document.body.style.backgroundImage = playingTheme.bgimage
     document.body.style.backgroundSize = "cover"
-    setInterval(function(){spawnEnemy(playingTheme)}, 1500)
+    setInterval(function(){spawnEnemy(playingTheme)},500)
+    setInterval(function(){gameLogic(playingTheme)}, 250)
+}
+
+function gameLogic(){
+    if(enemyIds.length !== 0) {
+        if(enemyCount === 1){
+            moveEnemies()
+        }else if(enemyIds.length === 2){
+            moveEnemies()
+            moveOrder++
+            if(moveOrder === 2){
+                moveOrder = 0
+            }
+        }else if(enemyIds.length === 3){
+            moveEnemies()
+            moveOrder++
+        }if (moveOrder === 3) {
+                moveOrder = 0
+        }
+    }
 }
 
 function getTheme() {
@@ -54,28 +78,61 @@ function getLevelData(playingTheme) {
 }
 
 function spawnEnemy(playingTheme){
-    if(enemyCount < 3){
+    if(enemyCount !== 3){
         let enemy = document.createElement('div');
-        enemy.setAttribute('id', `enemy${enemyCount}`)
+        enemy.setAttribute('id', `enemy${spawnedEnemies}`)
         let randomEnemy = playingTheme.enemies[Math.floor(Math.random() * 2)]
-        enemy.innerHTML = `<img src="/static/pictures/${theme}/${randomEnemy}" id='enemy${enemyCount}' style="height: 250px;"/>`;
+        enemy.innerHTML = `<img src="/static/pictures/${theme}/${randomEnemy}" id='enemy${spawnedEnemies}' style="height: 100px;"/>`;
         battleGround.appendChild(enemy);
         let enemyWord = document.createElement('span');
         enemyWord.setAttribute('class', 'caption')
-        let newEnemy = document.getElementById(`enemy${enemyCount}`);
+        let newEnemy = document.getElementById(`enemy${spawnedEnemies}`);
         enemyWord.innerHTML = getRandomWord();
         newEnemy.appendChild(enemyWord);
+        if(enemyIds.length > 0){
+            enemyIds.push(`enemy${enemyCount}`)
+
+        }else{
+            enemyIds.unshift(`enemy${enemyCount}`)
+        }
         enemyCount++;
+        console.log(`Enemy ${spawnedEnemies} spawned!`)
+        spawnedEnemies++;
+        if(spawnedEnemies === 3){
+            spawnedEnemies = 0
+        }
     }
 }
 
 
 function getRandomWord(){
     let difficulties = {'easy': words.easyWords, 'normal': words.normalWords, 'boss': words.bossWords}
-    console.log(difficulties[difficulty][Math.floor(Math.random() * difficulties[difficulty].length)])
     return difficulties[difficulty][Math.floor(Math.random() * difficulties[difficulty].length)]
 
 }
 
+function moveEnemies(){
+    let enemyToMove = enemyIds.indexOf(`enemy${moveOrder}`)
+    let selectedEnemy = document.getElementById(enemyIds[enemyToMove])
+    selectedEnemy.children[0].style.height = addToHeight(selectedEnemy.children[0].style.height)
+    enemyHeightCheck()
+}
+function addToHeight(height){
+    height = height.replace("px","")
+    height = parseInt(height)
+    height += 10
+    height = `${height}px`
+    return height
+}
 
+function enemyHeightCheck(){
+    let enemyToMove = enemyIds.indexOf(`enemy${moveOrder}`)
+    let selectedEnemy = document.getElementById(enemyIds[enemyToMove])
+    if (selectedEnemy.children[0].style.height === "200px") {
+        battleGround.removeChild(selectedEnemy)
+        let enemyToRemove = enemyIds.indexOf(`enemy${moveOrder}`)
+        enemyIds.splice(enemyToRemove)
+        enemyCount--
+    }
+}
 window.onload = initGame
