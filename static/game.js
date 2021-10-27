b786b2bc9991946d057d1483d86e5564feff69e2
Csupihun <csupihun@gmail.com>
2021-10-27 08:55:15 +0200

    add spawning enemies

--- static/game.js
+++ static/game.js
@@ -1,16 +1,60 @@
 // initGame();
 import * as enemies from "./enemies.js"
 
+const theme = document.getElementById('theme/lvl').innerText.split(' ')[0]
+const level = document.getElementById('theme/lvl').innerText.split(' ')[1]
+const battleGround = document.getElementById("battleground")
+// level resources
+const lotr = {enemy: 'troll.png', lvl1: 'BackgroundOne.png'}
+const shrek = {}
+const marvel = {}
+let enemyCount = 0
+let order = {0: '300', 1: '150', 2: '0'}
+
+
 function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
-    create_menu()
-    enemies.create_enemy()
+    let playingTheme = getTheme()
+    playingTheme = getLevelBackground(playingTheme)
+    document.body.style.backgroundImage = `url("/static/pictures/${theme}/${playingTheme.bgimage}")`
+    document.body.style.backgroundImage = playingTheme.bgimage
+    document.body.style.backgroundSize = "cover"
+    setInterval(function(){spawnEnemy(playingTheme)}, 1500)
 }
 
-function create_menu(){
-    let body = document.body
+function getTheme() {
+    if(theme === "LOTR"){
+        return lotr
+    }
+    else if(theme === "Avengers"){
+        return marvel
+    }
+    else if(theme === "Shrek"){
+        return shrek
+    }
 }
 
+function getLevelBackground(playingTheme) {
+    if(level === 'LVL1'){
+        playingTheme.bgimage = playingTheme.lvl1
+        return playingTheme
+    }
+    else if(level === 'LVL2'){
+        playingTheme.bgimage = playingTheme.lvl1
+        return playingTheme
+    }
+    else if(level === 'LVL3'){
+        playingTheme.bgimage = playingTheme.lvl1
+        return playingTheme
+    }
+}
+
+function spawnEnemy(playingTheme){
+    if(enemyCount < 3){
+        let enemy = document.createElement('div')
+        enemy.innerHTML = `<img src="/static/pictures/${theme}/${playingTheme.enemy}" id='enemy${enemyCount}' style="height: 250px; right: ${order[enemyCount]};" />`
+        battleGround.appendChild(enemy)
+        enemyCount++
+    }
+}
 window.onload = initGame
-
-
